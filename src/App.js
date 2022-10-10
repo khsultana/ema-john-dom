d9a9080a0f3092bb501ce330850cd8c8145e0845
Khan Sultana <Khan.Sultana@fifo-tech.com>
2022-10-10 10:10:43 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Main from './Layout/Main';
+
+
+function App() {
+  const router = createBrowserRouter([
+
+    { path: '/', element: <Main></Main> }
+  ])
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
